094b76fbb0936fb5612a23905a52be8fd962213f
Robin <robin071196@gmail.com>
2019-01-08 21:49:31 +0700

    Login File

---
+++ src/main/java/com/future/phase2/tugas/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.future.phase2.tugas.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LoginController {
+
+}
---
+++ src/main/java/com/future/phase2/tugas/service/LoginServiceImpl.java
@@ -0,0 +1,4 @@
+package com.future.phase2.tugas.service;
+
+public class LoginServiceImpl implements LoginService {
+}
